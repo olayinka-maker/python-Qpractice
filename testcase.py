44f1c569acdbcd51d1bb31abd3036f70d148c8d0
olayinka-maker <ajibodeolayinka09@gmail.com>
2024-09-21 04:19:24 +0100

    added a  new file for course update

---
+++ testcase.py
@@ -0,0 +1,16 @@
+#formatted String
+first = "Hello"
+last = "World"
+full = f"{first} {last}"
+
+print(full)
+
+
+#useful string methods
+course = 'Python Programming'
+print(course.upper())
+print(course.strip())
+print(course.title())
+print(course.find("pro"))
+print(course.replace("P","H"))
+

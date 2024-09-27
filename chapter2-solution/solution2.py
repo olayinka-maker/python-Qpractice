778db65489066f7945a2b80c8b8d7642c9ad1d08
olayinka-maker <ajibodeolayinka09@gmail.com>
2024-09-27 15:58:17 +0100

    .practiced new concepts in pythn

--- chapter2-solution/solution2.py
+++ chapter2-solution/solution2.py
@@ -116,13 +116,14 @@ print()
 # 9. The Fibonacci numbers are the sequence below, where the first two numbers are 1, and each
 # number thereafter is the sum of the two preceding numbers. Write a program that asks the
 # user how many Fibonacci numbers to print and then prints that many.
-1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 . 
+# 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
 
 n = int(input("How many fibonacci number do you want print : "))
 a,b = 1,1
 
 print(a, end=" ")
 print(b, end=" ")
+print("c")
 
 for i in range(n-2):
   next_value = a + b
--- testcase.py
+++ testcase.py
@@ -39,58 +39,90 @@
 
 #List Unpacking
 
-numbers = [1,2,3]
-first,second,third = numbers 
+# numbers = [1,2,3]
+# first,second,third = numbers 
 
-print(first)
+# print(first)
 
-numbers_2 = [1,2,2,3,4,5,5,6,6,7,6]
+# numbers_2 = [1,2,2,3,4,5,5,6,6,7,6]
 
-first,second,*rest ,other= numbers_2
+# first,second,*rest ,other= numbers_2
 
-print(other)
-
-
-letters = ["a","b","c"]
-
-for index,value in enumerate(letters):
-    print(index,value)
-
- #adding an item to a list
-letters.append("d")
-
-#adding an item to a list at a particular position
-letters.insert(0,"cooked")
-print(letters)
-
-#remove an item at the end of a list
-letters.pop()
-letters.pop(0)
-
-#removing an item but you dont know the index 
-
-letters.remove("b") #removes the first occurence of b in the list 
+# print(other)
 
 
-test= ["1","2","3","4","5"]
-# del is used to remove a  range of items 
-del test[0:2]
-print(test)
+# letters = ["a","b","c"]
+
+# for index,value in enumerate(letters):
+#     print(index,value)
+
+#  #adding an item to a list
+# letters.append("d")
+
+# #adding an item to a list at a particular position
+# letters.insert(0,"cooked")
+# print(letters)
+
+# #remove an item at the end of a list
+# letters.pop()
+# letters.pop(0)
+
+# #removing an item but you dont know the index 
+
+# letters.remove("b") #removes the first occurence of b in the list 
 
 
-#finding the index of a data in a list 
+# test= ["1","2","3","4","5"]
+# # del is used to remove a  range of items 
+# del test[0:2]
+# print(test)
 
 
-testList =["a","b","c"]
-if "d" in testList:
-    print(testList.index("b"))
+# #finding the index of a data in a list 
 
 
-#sorting a List 
-lsort = [1,8,9,3,7,2]
-lsort.sort()
-print(lsort)
-print(sorted(lsort,reverse=True))
+# testList =["a","b","c"]
+# if "d" in testList:
+#     print(testList.index("b"))
+
+
+# #sorting a List 
+# lsort = [1,8,9,3,7,2]
+# lsort.sort()
+# print(lsort)
+# print(sorted(lsort,reverse=True))
 
 
 #lamda expression or function
+
+x = lambda a : a + 2
+print(x(5))
+
+#multiple args 
+x = lambda a,b,c: a +b+c+ 2
+print(x(5,3,4))
+
+
+#using lambda with  map functon
+items = [("prd1",10),("prd3",10),("prd2",12)]
+for item in items:
+    print(item)
+data = list(map(lambda item : item[1], items))
+print(data)
+
+
+#filter with lambda
+items = [("prd1",10),("prd3",9),("prd2",12)]
+for item in items:
+    if(item[1] >= 10):
+        print(item[1])
+## now using filter function
+
+filteredData = list(filter( lambda item : item[1] >= 10,items))
+print(filteredData)
+
+#list Comprehension
+
+filteredData = [item[1] for item in items]
+print(filteredData)
+

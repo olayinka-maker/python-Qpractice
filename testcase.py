07ac926dd5df5664e23655ae45e1c4adb61652bc
olayinka-maker <ajibodeolayinka09@gmail.com>
2024-09-29 23:43:50 +0100

    covered up on dictionaries in python

--- testcase.py
+++ testcase.py
@@ -95,91 +95,98 @@
 
 #lamda expression or function
 
-x = lambda a : a + 2
-print(x(5))
+# x = lambda a : a + 2
+# print(x(5))
 
-#multiple args 
-x = lambda a,b,c: a +b+c+ 2
-print(x(5,3,4))
-
-
-#using lambda with  map functon
-items = [("prd1",10),("prd3",10),("prd2",12)]
-for item in items:
-    print(item)
-data = list(map(lambda item : item[1], items))
-print(data)
+# #multiple args 
+# x = lambda a,b,c: a +b+c+ 2 
+# print(x(5,3,4))
 
 
-#filter with lambda
-items = [("prd1",10),("prd3",9),("prd2",12)]
-for item in items:
-    if(item[1] >= 10):
-        print(item[1])
+# #using lambda with  map functon
+# items = [("prd1",10),("prd3",10),("prd2",12)]
+# for item in items:
+#     print(item)
+# data = list(map(lambda item : item[1], items))
+# print(data)
+
+
+# #filter with lambda
+# items = [("prd1",10),("prd3",9),("prd2",12)]
+# for item in items:
+#     if(item[1] >= 10):
+#         print(item[1])
 
 
 
-## now using filter function
+# ## now using filter function
 
-filteredData = list(filter( lambda item : item[1] >= 10,items))
-print(filteredData)
+# filteredData = list(filter( lambda item : item[1] >= 10,items))
+# print(filteredData)
 
-##using list comprehesion to achieve the tsk above
-newFilteredData = [item for item in items if item[1] >= 10]
-print(f"data ${newFilteredData}")
-
-
-#list Comprehension
-
-filteredData = [item[1] for item in items]
-print(filteredData)
+# ##using list comprehesion to achieve the tsk above
+# newFilteredData = [item for item in items if item[1] >= 10]
+# print(f"data ${newFilteredData}")
 
 
-list1 =  [1,2,3]
-list2 = [10,20,30]
+# #list Comprehension
 
-x = zip("aeebc",list1,list2)
-print(list(x))
+# filteredData = [item[1] for item in items]
+# print(filteredData)
 
-#stack in python
+
+# list1 =  [1,2,3]
+# list2 = [10,20,30]
+
+# x = zip("aeebc",list1,list2)
+# print(list(x))
+
+# #stack in python
  
-testlist = [1,2,3]
-print(testlist[-1])
+# testlist = [1,2,3]
+# print(testlist[-1])
 
 
-#queue
+# #queue
 
-from collections import deque
-queue = deque([])
-queue.append(1)
-queue.append(2)
-queue.append(3)
-queue.popleft()
-print(queue)
-if  not queue:
-    print("empty")
+# from collections import deque
+# queue = deque([])
+# queue.append(1)
+# queue.append(2)
+# queue.append(3)
+# queue.popleft()
+# print(queue)
+# if  not queue:
+#     print("empty")
 
-#dictionary examples
-d = {'dog' : 'has a tail and goes woof!',
-'cat' : 'says meow',
-'mouse' : 'chased by cats'}
+# #dictionary examples
+# d = {'dog' : 'has a tail and goes woof!',
+# 'cat' : 'says meow',
+# 'mouse' : 'chased by cats'}
 
-word = input('Enter a word : ')
-print('The definition is:', d[word])
-
-
-
-points = {'A':1, 'B':3, 'C':3, 'D':2, 'E':1, 'F':4, 'G':2,
-'H':4, 'I':1, 'J':8, 'K':5, 'L':1, 'M':3, 'N':1,
-'O':1, 'P':3, 'Q':10, 'R':1, 'S':1, 'T':1, 'U':1,
-'V':4, 'W':4, 'X':8, 'Y':4, 'Z':10}
-score = sum([points[c] for c in points])
-print(score)
+# word = input('Enter a word : ')
+# print('The definition is:', d[word])
 
 
-deck = [{'value':i, 'suit':c}
-for c in ['spades', 'clubs', 'hearts', 'diamonds']
-for i in range(2,15)]
 
-prin = deck[0]["value"]
-print(deck)
+# points = {'A':1, 'B':3, 'C':3, 'D':2, 'E':1, 'F':4, 'G':2,
+# 'H':4, 'I':1, 'J':8, 'K':5, 'L':1, 'M':3, 'N':1,
+# 'O':1, 'P':3, 'Q':10, 'R':1, 'S':1, 'T':1, 'U':1,
+# 'V':4, 'W':4, 'X':8, 'Y':4, 'Z':10}
+# score = sum([points[c] for c in points])
+# print(score)
+
+
+# deck = [{'value':i, 'suit':c}
+# for c in ['spades', 'clubs', 'hearts', 'diamonds']
+# for i in range(2,15)]
+
+# prin = deck[0]["value"]
+# print(deck)
+
+
+# d = {'A':100, 'B':200,'C':100,}
+# value = [x[1] for x in d.items() if x[1] == 100 ]
+# print(value)
+
+

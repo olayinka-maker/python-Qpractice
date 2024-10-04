7583ccc736913aaa229723af535f79a51c66e52a
olayinka-maker <ajibodeolayinka09@gmail.com>
2024-10-04 14:05:32 +0100

    test2

--- chapterdictionary/soln.py
+++ chapterdictionary/soln.py
@@ -51,7 +51,17 @@
 
 #while loop
 
-i = 1
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 5:
+#     break 
+#   i += 1
+
+
+i = 0
 while i < 6:
-  print(i)
   i += 1
+  if i == 3:
+    continue 
+  print(i)
